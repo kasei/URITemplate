fc85f6e3bd3b66aa38f4a0b2688ef982e52849f3
Gregory Todd Williams <greg@evilfunhouse.com>
2018-09-20 10:06:44 -0700

    Updated dependency versions.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name: "URITemplate",
   dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
-    .Package(url: "https://github.com/kylef/PathKit", majorVersion: 0, minor: 7),
+    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 9),
+    .Package(url: "https://github.com/kylef/PathKit", majorVersion: 0, minor: 9),
   ],
   swiftLanguageVersions: [3, 4]
 )
