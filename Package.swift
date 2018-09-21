34fcc5071172eeea52f66055c2b54d33f9552d51
Gregory Todd Williams <greg@evilfunhouse.com>
2018-09-21 09:32:05 -0700

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
   name: "URITemplate",
+  products: [
+  	.library(name: "URITemplate", targets: ["URITemplate"])
+  ],
   dependencies: [
     .package(url: "https://github.com/kylef/Spectre", from: "0.9.0"),
     .package(url: "https://github.com/kylef/PathKit", from: "0.9.0"),
